fb9e52c7438e771c6879fa2e5d32f2540acadc0d
catoverthebench <102893564+Axisstorm110@users.noreply.github.com>
2026-04-19 12:57:03 -0400

    Delete src directory

--- src/mcguard/__init__.py
+++
@@ -1,6 +0,0 @@
-from .plan import ResamplingPlan
-from .bootstrap import bootstrap_ci
-from .permutation import permutation_test
-from . import metrics
-
-__all__ = ["ResamplingPlan", "bootstrap_ci", "permutation_test", "metrics"]
